aa5973dfe82d873981108802ea783277ef6413a8
Eslam Abdelhady <65198520+E-Hady@users.noreply.github.com>
2023-07-08 15:42:36 +0300

    Update and rename chatbot.py to app.py

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+from machine_learning_model import predict_disease
+
+# Flask app
+app = Flask(__name__)
+
+# Load the symptom data
+data = pd.read_csv('unique_symptoms.csv')
+
+base_list = []
+check_list = []
+asked_list = []
+features = {}
+
+# Function to select rows based on selected symptoms
+most_common_symptom = data.iloc[:, 1:].stack().value_counts().idxmax()
+
+def select_rows(data, base_list):
+    mask = data.apply(lambda x: all(symptom in x.values.astype(str) for symptom in base_list), axis=1)
+    selected_rows = data[mask]
+    return selected_rows
+
+
+# Welcome message
+welcome_message = "Welcome to the Chatbot. Please enter 'yes' or 'no' for the following questions. Enter 'Exit' to end the chat."
+
+# Webhook endpoint
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    request_data = request.get_json()
+    response = ""
+
+    # Extract the necessary information from the request JSON
+    intent = request_data['queryResult']['intent']['displayName']
+    parameters = request_data['queryResult']['parameters']
+
+    if intent == 'start':
+        response = welcome_message
+
+    elif intent == 'exit':
+        symptoms = parameters['symptoms']
+        response = predict_disease(symptoms)
+
+    elif intent == 'yes':
+        symptom = parameters['symptom']
+        # Process the 'yes' response from the user
+        base_list.append(symptom)
+        asked_list.append(symptom)
+        features[symptom] = 1
+        check_list = [symptom for symptom in check_list if symptom not in base_list]
+        # Generate the appropriate response for the next question or prompt
+
+    elif intent == 'no':
+        symptom = parameters['symptom']
+        # Process the 'no' response from the user
+        asked_list.append(symptom)
+        features[symptom] = 0
+        check_list = [symptom for symptom in check_list if symptom not in base_list]
+        # Generate the appropriate response for the next question or prompt
+
+    else:
+        response = "Sorry, I didn't understand that."
+
+    # Create the webhook response
+    webhook_response = {
+        "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [response]
+                }
+            }
+        ]
+    }
+
+    return jsonify(webhook_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
