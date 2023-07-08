2999437d74c897c6218cb6ed3dc82345d426fd1f
Eslam Abdelhady <65198520+E-Hady@users.noreply.github.com>
2023-07-08 15:50:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,12 @@ base_list = []
 check_list = []
 asked_list = []
 features = {}
-
 # Function to select rows based on selected symptoms
-most_common_symptom = data.iloc[:, 1:].stack().value_counts().idxmax()
 
 def select_rows(data, base_list):
     mask = data.apply(lambda x: all(symptom in x.values.astype(str) for symptom in base_list), axis=1)
     selected_rows = data[mask]
     return selected_rows
-
 
 # Welcome message
 welcome_message = "Welcome to the Chatbot. Please enter 'yes' or 'no' for the following questions. Enter 'Exit' to end the chat."
@@ -38,25 +35,34 @@ def webhook():
         response = welcome_message
 
     elif intent == 'exit':
-        symptoms = parameters['symptoms']
-        response = predict_disease(symptoms)
+        symptoms = parameters.get('symptoms', [])
+        if symptoms:
+            response = predict_disease(symptoms)
+        else:
+            response = "Please provide the symptoms for prediction."
 
     elif intent == 'yes':
-        symptom = parameters['symptom']
-        # Process the 'yes' response from the user
-        base_list.append(symptom)
-        asked_list.append(symptom)
-        features[symptom] = 1
-        check_list = [symptom for symptom in check_list if symptom not in base_list]
-        # Generate the appropriate response for the next question or prompt
+        symptom = parameters.get('symptom')
+        if symptom:
+            # Process the 'yes' response from the user
+            base_list.append(symptom)
+            asked_list.append(symptom)
+            features[symptom] = 1
+            check_list = [symptom for symptom in check_list if symptom not in base_list]
+            # Generate the appropriate response for the next question or prompt
+        else:
+            response = "Please provide a symptom for the 'yes' response."
 
     elif intent == 'no':
-        symptom = parameters['symptom']
-        # Process the 'no' response from the user
-        asked_list.append(symptom)
-        features[symptom] = 0
-        check_list = [symptom for symptom in check_list if symptom not in base_list]
-        # Generate the appropriate response for the next question or prompt
+        symptom = parameters.get('symptom')
+        if symptom:
+            # Process the 'no' response from the user
+            asked_list.append(symptom)
+            features[symptom] = 0
+            check_list = [symptom for symptom in check_list if symptom not in base_list]
+            # Generate the appropriate response for the next question or prompt
+        else:
+            response = "Please provide a symptom for the 'no' response."
 
     else:
         response = "Sorry, I didn't understand that."
